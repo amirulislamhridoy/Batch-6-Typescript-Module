18a0685dd02a796ba94a5d8a1bddd51e1eb38880
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2023-03-03 00:41:36 +0600

    interface & extends

---
+++ video-8 interface & alias vs interface difference/tsFiles/interfaceWithDifference.ts
@@ -0,0 +1,53 @@
+// interface
+// interface use for generally object & function, interface extends, type aliases extends
+
+// inteface with object
+interface Person2 {
+    name: string, age: number | string
+}
+let person2: Person2;
+person2 = {name: 's', age: 's'}
+interface Address2{
+    present: string,
+    permanent: string
+} 
+interface Per {
+    name: string, 
+    address: Address
+}
+let per: Per = {name: 's', address: {
+    present: 'a', permanent: 'oo'
+}}
+
+// interface with function
+interface Add {
+    (a: number, b?: number): number;
+}
+const add : Add = (a, b) => {
+    if(b){
+        return a + b
+    }else{
+        return a
+    }
+}
+
+// extends
+// interface extends
+interface auth extends Address2 {
+    o: string
+}
+let Auth: auth;
+Auth = {
+    o: '',
+    present: 'a',
+    permanent: 'a'
+}
+
+// type aliases extends
+type a = {name: string}
+type b = {
+    old: string
+} & a
+let bb: b = {
+    name: '', old: 'oo'
+}
